6925553693fe6a7263cc2d78ac144131cc0059f8
kapoorritesh1111-create <kapoorritesh1111@gmail.com>
2026-05-25 13:25:04 -0400

    28 a, b and c

--- src/lib/queries/leads.ts
+++ src/lib/queries/leads.ts
@@ -1,7 +1,2 @@
-<<<<<<< Updated upstream
-export type { LeadProfileData, LeadsPageData } from './data';
-export { getLeadProfileData, getLeadsPageData } from './data';
-=======
 export type { LeadProfileData, LeadsPageData } from './query-core';
 export { getLeadProfileData, getLeadsPageData } from './query-core';
->>>>>>> Stashed changes
--- src/lib/queries/pipeline.ts
+++ src/lib/queries/pipeline.ts
@@ -1,7 +1,2 @@
-<<<<<<< Updated upstream
-export type { LeadsPageData, PipelineData } from './data';
-export { getLeadsPageData as getPipelinePageData } from './data';
-=======
 export type { LeadsPageData, PipelineData } from './query-core';
 export { getLeadsPageData as getPipelinePageData } from './query-core';
->>>>>>> Stashed changes
