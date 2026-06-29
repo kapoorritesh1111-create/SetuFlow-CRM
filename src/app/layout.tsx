4d766f09d2e137c4db53fa5e94ea740081336e61
Ritesh Kapoor <kapoorritesh1111@gmail.com>
2026-06-29 18:11:31 -0400

    Fix documents preview build by removing unresolved products polish import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,6 @@ import { Plus_Jakarta_Sans } from 'next/font/google';
 import { ServiceWorkerRegistration } from '@/components/shell/ServiceWorkerRegistration';
 import { LeadsFilterStability } from '@/components/shell/LeadsFilterStability';
 import { OfflineStatusBanner } from '@/components/shell/OfflineStatusBanner';
-import { ProductsUiPolish } from '@/components/shell/ProductsUiPolish';
 import { DocumentsUiPolish } from '@/components/shell/DocumentsUiPolish';
 
 const jakarta = Plus_Jakarta_Sans({
@@ -146,7 +145,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <ServiceWorkerRegistration />
         <LeadsFilterStability />
         <OfflineStatusBanner />
-        <ProductsUiPolish />
         <DocumentsUiPolish />
         {children}
       </body>
