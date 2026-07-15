690c8406edb087992113bca21de77708a12beea1
Ritesh Kapoor <kapoorritesh1111@gmail.com>
2026-07-15 08:41:43 -0400

    feat: add compact connected Product Overview hub

---
+++ src/components/marketing/product-overview-hub.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+import {
+  ArrowRight,
+  BarChart3,
+  BookOpenCheck,
+  BriefcaseBusiness,
+  Building2,
+  CalendarDays,
+  Check,
+  CircleDollarSign,
+  ClipboardCheck,
+  ContactRound,
+  FileText,
+  MessageCircleMore,
+  PackageCheck,
+  ScanLine,
+  Search,
+  Send,
+  ShieldCheck,
+  ShoppingBag,
+  Store,
+  Truck,
+  Users
